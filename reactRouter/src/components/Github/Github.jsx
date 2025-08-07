51377a521fa2ca7bb5eb9a8e09670231d5c636bd
pri2405 <psharma532003@gmail.com>
2025-08-07 22:06:22 +0530

    routing part1

---
+++ reactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,7 @@
+function Github() {
+    return (
+        <div className="text-center bg-gray-600 text-white m-3 text-3xl p-4">Github Profile</div>
+    )
+}
+
+export default Github;
---
+++ reactRouter/src/components/User/User.jsx
@@ -0,0 +1,15 @@
+import { useParams } from "react-router";
+import Github from "../Github/Github";
+
+function User() {
+    const {userid} = useParams()
+    return (
+        <div>
+            <div className="text-3xl bg-gray-700 text-white text-center p-4 m-4">User: {userid}</div>
+            <Github />
+        </div>
+        
+    )
+}
+
+export default User;
