e3368324dabd10d39bffa5415e22fd2dc5a9a074
pri2405 <psharma532003@gmail.com>
2025-08-12 16:51:25 +0530

    complete project with local storage

--- todoContextLocal/src/App.jsx
+++ todoContextLocal/src/App.jsx
@@ -1,5 +1,5 @@
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import {TodoProvider} from './contexts/index'
 import './App.css'
 import TodoForm from './components/TodoForm'
@@ -24,10 +24,21 @@ function App() {
     setTodos((prev) => prev.map((prevTodo) => prevTodo.id === id ? {...prevTodo, completed: !prevTodo.completed} : prevTodo))
   }
 
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todos"))
+
+    if(todos && todos.length > 0) {
+      setTodos(todos)
+    } 
+  },[])
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos))
+  }, [todos])
 
   return (
     <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleComplete}}>
-      <div className="bg-[#172842] min-h-screen py-8">
+      <div className="w-full bg-[#172842] min-h-screen py-8">
           <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
               <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
               <div className="mb-4">
@@ -35,9 +46,11 @@ function App() {
               </div>
               <div className="flex flex-wrap gap-y-3">
                   {
-                    todos.map((todo) => {
-                      <TodoItem todo={todo} />
-                    })
+                    todos.map((todo) => (
+                      <div key={todo.id} className=''>
+                        <TodoItem todo={todo} />
+                      </div>
+                    ))
                   }
               </div>
           </div>
--- todoContextLocal/src/components/TodoItem.jsx
+++ todoContextLocal/src/components/TodoItem.jsx
@@ -1,5 +1,19 @@
+import { useState } from "react";
+import { useTodo } from "../contexts";
+
 function TodoItem({ todo }) {
-    
+    const [isTodoEditable, setIsTodoEditable] = useState(false)
+    const [todoMsg, setTodoMsg] = useState(todo.todo)
+    const {updateTodo, deleteTodo, toggleComplete} = useTodo()
+
+    const editTodo = () => {
+       updateTodo(todo.id, {...todo, todo: todoMsg})
+       setIsTodoEditable(false)
+    }
+
+    const toggleCompleted = () => {
+        toggleComplete(todo.id)
+    }
 
     return (
         <div
@@ -24,7 +38,7 @@ function TodoItem({ todo }) {
             />
             {/* Edit, Save Button */}
             <button
-                className="inline-flex w-8 h-8 rounded-lg text-sm border border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0 disabled:opacity-50"
+                className={`inline-flex w-8 h-8 rounded-lg text-sm border border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0 disabled:opacity-50 ${todo.completed ? "" : "cursor-pointer"}`}
                 onClick={() => {
                     if (todo.completed) return;
 
@@ -38,7 +52,7 @@ function TodoItem({ todo }) {
             </button>
             {/* Delete Todo Button */}
             <button
-                className="inline-flex w-8 h-8 rounded-lg text-sm border border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0"
+                className="cursor-pointer inline-flex w-8 h-8 rounded-lg text-sm border border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0"
                 onClick={() => deleteTodo(todo.id)}
             >
                 ❌
