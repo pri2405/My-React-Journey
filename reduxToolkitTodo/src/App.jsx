1293c992eebd0f34dbe5abb66df220e332161a43
pri2405 <psharma532003@gmail.com>
2025-08-17 20:31:37 +0530

    final part submit

--- reduxToolkitTodo/src/App.jsx
+++ reduxToolkitTodo/src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from 'react'
 
 import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Rdux Toolkit</h1>
+      <h1 className='text-4xl font-bold text-center p-4'>Todo App with Redux Toolkit</h1>
+      <AddTodo />
+      <Todos />
     </>
   )
 }
